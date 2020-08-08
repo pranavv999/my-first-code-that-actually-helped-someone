b9d6a09ccd1f578025d9f9159925508fe3cb41ee
Pranav Sonawane <prsaso999@gmail.com>
2020-08-08 18:31:11 +0530

    Update extract_entries.py

--- extract_entries.py
+++ extract_entries.py
@@ -1,1 +1,20 @@
+with open('kaka_BC OUT (1).txt', mode='r') as file:
+	with open('final_entries.txt', mode='w') as data:
+
+		for line in file.readlines():
+
+			a = line.split()
+			b = ''
+			
+			if (len(a) > 0):
+
+				for i in a[-1]:
+					if (i == ','):
+						pass
+					else:
+						b = b + i
+
+				if (float(b) > 999):
+					data.write(' '.join(a))
+					data.write('\r\n')
 
